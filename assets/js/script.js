e0db5e823ca78048b68949cee422a03b21be8f32
Eric <ericphilipps123@gmail.com>
2022-11-16 12:27:18 -0600

    JS for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,14 @@ const temp = $('#temp');
 const wind = $('#wind');
 const humidity = $('#humidity');
 const uv = $('#uv');
+const currIcon = $('#current-icon');
+const description = $('#description');
+const cityTitle = $('#city-title');
+
 let city = ""
 
 function currentWeather (city) {
     let query = "http://api.openweathermap.org/geo/1.0/direct?q=" +city+"&limit=5" +"&appid=" +myKey;
-    console.log(query);
     fetch(query)
     .then((response) => {
         console.log(response);
@@ -21,22 +24,31 @@ function currentWeather (city) {
 
         let secondQuery = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&limit=5&units=imperial&appid=" +myKey;
         
+        console.log(secondQuery);
         fetch(secondQuery)
         .then ((response) => {
             return response.json();
         })
         .then((data) => {
+            currIcon.attr('src', `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`);
             temp.text("Temperature: " + data.current.temp + "F");
             wind.text("Wind Speed: " + data.current.wind_speed + "MPH");
             humidity.text("Humidity: " + data.current.humidity + "%");
             uv.text("UV Index: " + data.current.uvi);
+            description.text(data.current.weather[0].description);
+            futureWeather(data);
         })
     })
+}
+
+function futureWeather (weatherData) {
+    
 }
 
 searchBtn.on("click", function(e) {
     e.preventDefault();
     city = searchedCity.val();
-    console.log('hello');
+    cityTitle.text(searchedCity.val());
     currentWeather(city);
+    
 })
