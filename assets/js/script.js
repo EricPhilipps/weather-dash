e9ccc1968ac705bb1290beb234ad4d974c5d39fb
Eric <ericphilipps123@gmail.com>
2022-11-16 15:01:46 -0600

    rough draft seven day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// const moment = require('moment');
+// example data url: https://api.openweathermap.org/data/2.5/onecall?lat=40.7127281&lon=-74.0060152&exclude=minutely,hourly,alerts&limit=5&units=imperial&appid=cc50518f37e2ddd6b048abeecab6ec3e
+
 const myKey = "cc50518f37e2ddd6b048abeecab6ec3e";
 const searchedCity = $('#search-location');
 const searchBtn = $('#search-btn');
@@ -8,6 +11,7 @@ const uv = $('#uv');
 const currIcon = $('#current-icon');
 const description = $('#description');
 const cityTitle = $('#city-title');
+const sevenDay = $("#seven-day")
 
 let city = ""
 
@@ -31,18 +35,53 @@ function currentWeather (city) {
         })
         .then((data) => {
             currIcon.attr('src', `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`);
+            description.text(data.current.weather[0].description);
             temp.text("Temperature: " + data.current.temp + "F");
-            wind.text("Wind Speed: " + data.current.wind_speed + "MPH");
+            wind.text("Wind Speed: " + Math.round(data.current.wind_speed) + "mph");
             humidity.text("Humidity: " + data.current.humidity + "%");
             uv.text("UV Index: " + data.current.uvi);
-            description.text(data.current.weather[0].description);
             futureWeather(data);
         })
     })
 }
 
 function futureWeather (weatherData) {
+    console.log(weatherData);
+
+    for (let i = 0; i < 6; i++) {
+        const futureInfo = {
+            iconCode: weatherData.daily[i].weather[0].icon,
+            date: weatherData.daily[i].dt,
+            temp: weatherData.daily[i].temp.max,
+            wind: Math.round(weatherData.daily[i].wind_speed),
+            humidity: weatherData.daily[i].humidity,
+            description: weatherData.daily[i].weather[0].main,
+            uv: weatherData.daily[i].uvi
+        }
+
+        console.log(futureInfo.iconCode);
+
+        let iconURL = `https://openweathermap.org/img/w/${futureInfo.iconCode}.png`
+
+        let futureDayForecast = $(`
+            <div class="card">
+                <img src="${iconURL}"/>
+                <div class="card-body">
+                    <h5 class="card-title" id=${"date" + i}>${futureInfo.date}</h5>
+                    <ul>
+                        <li id=${"description" + i}>${futureInfo.description}</li>
+                        <li id=${"temp" + i}>Temperature: ${futureInfo.temp} F</li>
+                        <li id=${"wind" + i}>Wind Speed: ${futureInfo.wind} mph</li>
+                        <li id=${"humidity" + i}>Humidity: ${futureInfo.humidity} %</li>
+                        <li id=${"uv" + i}>UV Index: ${futureInfo.uv}</li>
+                    </ul>
+                </div>
+            </div>
+        `);
     
+        sevenDay.append(futureDayForecast);
+
+    }
 }
 
 searchBtn.on("click", function(e) {
